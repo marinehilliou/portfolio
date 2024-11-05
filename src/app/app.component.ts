25919a3126bb208aa3ba5c97f100deb74dc25ff2
marinehilliou <marine.hilliou@orange.fr>
2024-11-05 16:17:31 +0100

    test for mobile menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,5 +33,10 @@ export class AppComponent implements AfterViewInit {
         }
       });
     });
+
+    document.getElementById('mobile-menu')?.addEventListener('click', function () {
+      this.classList.toggle('active');
+      document.querySelector('.nav-links')?.classList.toggle('active');
+    });
   }
 }
